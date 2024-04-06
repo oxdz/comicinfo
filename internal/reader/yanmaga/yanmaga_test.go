6e257f5fef2e320f2851c20d149b1999e6671503
vircoys <vircoys@gmail.com>
2024-04-06 23:57:47 +0800

    yanmanga random query parameter k

---
+++ internal/reader/yanmaga/yanmaga_test.go
@@ -0,0 +1,8 @@
+package yanmaga
+
+import "testing"
+
+func TestRandomK(t *testing.T) {
+	s := randomK("06A0000000000711626D")
+	t.Log(s)
+}
--- internal/reader/yanmaga/yanmanga.go
+++ internal/reader/yanmaga/yanmanga.go
@@ -2,11 +2,18 @@ package yanmaga
 
 import (
 	"context"
+	"crypto/rand"
+	"math"
+	"math/big"
 
 	"github.com/oxdz/comicinfo/internal/reader"
 )
 
 var _ reader.ComicReader = (*Viewer)(nil)
+
+const (
+	defaultRandomStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+)
 
 type Viewer struct {
 }
@@ -21,4 +28,50 @@ func (v *Viewer) EpisodeInfo(ctx context.Context) (*reader.EpisodeInfo, error) {
 
 func (v *Viewer) DownloadPags(ctx context.Context, url string) (<-chan *reader.PageData, error) {
 	return nil, nil
+}
+
+func randomStr(n int, s string) string {
+	if s == "" {
+		s = defaultRandomStr
+	}
+
+	var r string
+	for i := 0; i < int(n); i++ {
+		v, _ := rand.Int(rand.Reader, big.NewInt(int64(len(s))))
+		if v != nil {
+			r += string(s[v.Int64()])
+		} else {
+			r += string(s[0])
+		}
+	}
+	return r
+}
+
+func randomK(cid string) string {
+	var li string
+	for i := 0; i < int(math.Ceil(16.0/float64(len(cid)))); i++ {
+		li += cid
+	}
+
+	subH := li[:16]
+
+	var subT string
+	if n := len(li) - 16; n < 10 {
+		subT = li[:16]
+	} else {
+		subT = li[n:16]
+	}
+
+	var v [3]int
+	str := randomStr(16, defaultRandomStr)
+
+	var ret string
+	for i, c := range []byte(str) {
+		v[0] ^= int(c)
+		v[1] ^= int(subH[i])
+		v[2] ^= int(subT[i])
+		ret += string(c) + string(defaultRandomStr[(v[0]+v[1]+v[2])&63])
+	}
+
+	return ret
 }
